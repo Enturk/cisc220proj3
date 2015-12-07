0119d6821e99bb2ab6d970b30098e916be427869
Samantha Hoffman <samahoffman@wifi-roaming-128-4-140-247.host.udel.edu>
2015-12-07 14:43:27 -0500

    board gen and structs changes

--- Scrabble/board/consumer.cpp
+++ Scrabble/board/consumer.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include weights.cpp
 using namespace std;
 
 /* Input data consumer functions
@@ -10,9 +11,10 @@ using namespace std;
  * that it does this in a way that is inline with the
  * expectations of the rest of the functions.
  */
+
 Board boardGen() {
     /* Returns a Board object
-     * This board is expected to be comprised of a 
+     * This board is expected to be comprised of a
      * one-dimensional array of tiles (board.tiles)
      *
      * About representing a 2D board in 1D.
@@ -21,13 +23,13 @@ Board boardGen() {
      * mathematical operation to "flatten" and "deflatten"
      * the board. It should be abstracted in such a way
      * that you can call board.set(0,0) = tile;
-     * 
+     *
      * more on that in the board struct.
      */
     ifstream inFile;
     string x;
     inFile.open("./board/board.txt");
-    
+
     Board board;
     board.tiles = new Tile[15*15];
 
@@ -39,35 +41,25 @@ Board boardGen() {
     inFile >> x; //skip the first line(the rack)
     int row=0;
     while(inFile >> x) {
-        int col=0;
+       countY = 0;
         // This loops through each line of inFile and stores it
         // in variable x for each iteration.
         //
-        //   note: the iterator line below looks scary. think of it as 
+        //   note: the iterator line below looks scary. think of it as
         //   "for each char it in x"
         for(string::iterator it = x.begin(); it!=x.end();++it){
-            /*
-             * TODO: create a tile for each character. This is the general scaffolding, it needs
-             *  to be completed though.
-             * keep track of column and row.
-             * put that tile on the board. (board.getTile(col, row) = new Tile(it);)
-             */
-            Tile tile;
-            if(0/* it is a letter */){//figure out how to test if a char is a letter.
-                tile.letter = *it;
-                tile.score = weight(*it); // nazim made a switching function for getting the int weights.
-                                          // That will have to be included somewhere in here. Put it in ./lib/
-                // we should probably also set bonus=0
-            } else tile.letter = 0;
-            if(0 /*it is a number */){//figure out how to test if a char is a number
-                tile.bonus = (int)*it;//bonus is the int equivalent of the char. Not sure how to cast
-                                      // a char to an int 
-            }
-            tile.coords = {col, row};//I don't know how to create an array on the fly.
-            board.getTile(col, row) = tile;
-            col++;
+          if (*it == "-"){
+            board.tiles.push_back(new Tile('',0,0, (countX,countY)));
+          }
+          if (isalpha(*it)){
+            board.tiles.push_back(new Tile(*it, 0, weight(*it), (countX,countY)));
+          }
+          if(isdigit(*it)){
+            board.tiles.push_back(new Tile('', *it, 0, (countX,countY)));
+          }
+        countY++;
         }
-        row++;
+        countX++;
     }
     inFile.close();
     return board;
@@ -85,20 +77,14 @@ Tile* rackGen() {
         return NULL;
     }
     inFile >> x; //Store the rack line in variable x;
-    
-    int i=0;
-    // for each char it in x 
+
+    // for each char it in x
     for(string::iterator it = x.begin(); it!=x.end();++it){
-        // TODO: Make this work. Find how to do if it==(any ascii letter, upper or lower) 
-        // create new tile, set its letter to it, set its score
         Tile tile;
-        if(0/* it is a letter */){
-            tile.letter = *it;
-            tile.score = weight(*it);
-            tile.coords = {-1}; // it's not on the board, coords shows that by {-1};
-            //probably should set tile.bonus = 0;
-        } else tile.letter = 0;
-        i++;
+        tile.letter = *it;
+        tile.score = weight(*it);
+        tile.coords = {-1}; // it's not on the board, coords shows that by {-1};
+        //probably should set tile.bonus = 0;
     }
     inFile.close();
     return tiles;
